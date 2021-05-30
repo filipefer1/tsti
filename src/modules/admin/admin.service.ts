4bd7611ab6cd174082030af2643d97790405759a
Filipe <filipe.fernandes@going2.com.br>
2021-05-30 19:57:01 -0300

    feat: add admin methods

--- src/modules/admin/admin.service.ts
+++ src/modules/admin/admin.service.ts
@@ -4,4 +4,12 @@ import { AdminRepository } from './admin.repository';
 @Injectable()
 export class AdminService {
     constructor(private readonly adminRepository: AdminRepository) {}
+
+    async findOne(id: string) {
+        return this.adminRepository.findOne(id);
+    }
+
+    async findAll() {
+        return this.adminRepository.find();
+    }
 }
