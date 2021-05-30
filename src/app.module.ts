10a86cd2507f6c17619e7ff5ab08865a42224c60
Filipe <filipe.fernandes@going2.com.br>
2021-05-30 19:55:05 -0300

    feat: add transaciontal middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ControllersModule } from './modules/controllers/controllers.module';
+import { BffModule } from './modules/bff/bff.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(), ControllersModule],
+    imports: [TypeOrmModule.forRoot(), BffModule],
     controllers: [AppController],
     providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
+import { initializeTransactionalContext } from 'typeorm-transactional-cls-hooked';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    initializeTransactionalContext();
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
